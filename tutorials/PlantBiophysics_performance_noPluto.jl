e0400f00fe6de130e8d79582ca039635c259381b
remi.vezy <VEZY@users.noreply.github.com>
2022-05-06 12:02:39 +0200

    Update benchmark figure

--- tutorials/PlantBiophysics_performance_noPluto.jl
+++ tutorials/PlantBiophysics_performance_noPluto.jl
@@ -141,13 +141,25 @@ end
 ######################################################################################################
 
 # Defining a structure to hold basic statistics
-struct stat_res
+struct StatResults
     mean::AbstractFloat
     median::AbstractFloat
     stddev::AbstractFloat
     min::AbstractFloat
     max::AbstractFloat
 end
+
+function Base.show(io::IO, ::MIME"text/plain", m::StatResults)
+    print(
+        io,
+        "Benchmark:",
+        "\nMean time -> ", m.mean, " ± ", m.stddev,
+        "\nMedian time -> ", m.median,
+        "\nMinimum time -> ", m.min,
+        "\nMaximum time -> ", m.max,
+    )
+end
+Base.show(io::IO, m::StatResults) = print(io, m.mean, "(±", m.stddev, ')')
 
 # Function computing the basic statistics
 function basic_stat(df)
@@ -156,7 +168,7 @@ function basic_stat(df)
     std = Statistics.std(df)
     min = findmin(df)[1]
     max = findmax(df)[1]
-    return stat_res(m, med, std, min, max)
+    return StatResults(m, med, std, min, max)
 end
 
 statsPB = basic_stat(time_PB)
@@ -170,39 +182,57 @@ factorLG = mean(time_LG) / mean(time_PB)
 # PLOTTING
 ######################################################################################################
 
-function plot_benchmark_Makie(statsPB, statsPE, statsLG)
+function plot_benchmark_Makie(statsPB, statsPE, statsLG, time_PB, time_PE, time_LG)
+    size_inches = (6.7, 5)
+    size_pt = 72 .* size_inches
+    bins = 220
     noto_sans = assetpath("fonts", "NotoSans-Regular.ttf")
-    fig = Makie.Figure(backgroundcolor=RGBf(1, 1, 1), resolution=(1000, 800), font=noto_sans, dpi=600, size=(1000, 1000), xminorgridstyle=true)
+    fig = Figure(backgroundcolor=RGBf(1, 1, 1), resolution=size_pt, font=noto_sans, fontsize=10)
     ep = 1e-9
-    interval = (3e-6, 2e-1) # x-axis
+    extr = extrema(vcat(time_PB, time_PE, time_LG))
+    interval = (extr[1] * 1e-1, extr[2])
 
-    axa = Axis(fig[1, 1:3], title="(a) PlantBiophysics.jl", yminorticks=IntervalsBetween(10), xscale=log10, xminorticks=IntervalsBetween(10), yminorgridvisible=true, yminorticksvisible=true, xminorgridvisible=true, xminorticksvisible=true)
-    stddevi = Makie.poly!(axa, Rect(max(ep, statsPB.mean - statsPB.stddev), 0.0, 2 * statsPB.stddev, 0.2), color=(:orange, 0.3))
-    Makie.hist!(axa, time_PB[time_PB.<1e-4], normalization=:probability, bins=300)
-    moy = Makie.lines!(axa, statsPB.mean * [1, 1], [0.0, 0.2], linewidth=3, linestyle=:dot, color=:red)
+    ga = fig[1, 1] = GridLayout()
+
+    axa = Axis(ga[1, 1], title="(a) PlantBiophysics.jl", xscale=log10, titlealign=:left, titlesize=10)
+    stddevi = poly!(
+        axa,
+        Rect(max(ep, statsPB.mean - statsPB.stddev), 0.0, 2 * statsPB.stddev, 1),
+        color=(:orange, 0.3), yautolimits=false
+    )
+    moy = vlines!(axa, statsPB.mean; color=:red, linewidth=3, linestyle=:dot)
+    hist!(axa, time_PB, normalization=:probability, bins=bins)
+    # h = axa.finallimits[].widths[2]
     axislegend(axa, [stddevi, moy], ["95% confidence interval", "Mean"], "", position=:rb,
-        orientation=:vertical, labelsize=13, framevisible=false)
-    Makie.xlims!(axa, interval)
+        orientation=:vertical, labelsize=8, framevisible=false)
+    xlims!(axa, interval)
 
-    axb = Axis(fig[2, 1:3], title="(b) plantecophys", yminorticks=IntervalsBetween(10), xscale=log10, xminorticks=IntervalsBetween(10), yminorgridvisible=true, yminorticksvisible=true, xminorgridvisible=true, xminorticksvisible=true)
-    stddevi = Makie.poly!(axb, Rect(statsPE.mean - statsPE.stddev, 0.0, 2 * statsPE.stddev, 0.2), color=(:orange, 0.3))
-    Makie.hist!(axb, time_PE[time_PE.<1e-0], normalization=:probability, bins=100)
-    moy = Makie.lines!(axb, statsPE.mean * [1, 1], [0.0, 0.2], linewidth=3, linestyle=:dot, color=:red)
-    #axislegend(axb, [stddevi,moy], ["95% confidence interval","Mean"], "", position = :rb,
-    #    orientation = :vertical,labelsize=13,framevisible = false)
-    Makie.xlims!(axb, interval)
+    axb = Axis(ga[2, 1], title="(b) plantecophys", xscale=log10, ylabel="Density", titlealign=:left, titlesize=10)
+    stddevi = poly!(
+        axb,
+        Rect(statsPE.mean - statsPE.stddev, 0.0, 2 * statsPE.stddev, 1),
+        color=(:orange, 0.3), yautolimits=false
+    )
+    vlines!(axb, statsPE.mean; color=:red, linewidth=3, linestyle=:dot)
+    hist!(axb, time_PE, normalization=:probability, bins=bins)
+    xlims!(axb, interval)
 
-    axc = Axis(fig[3, 1:3], title="(c) LeafGasExchange.jl", yminorticks=IntervalsBetween(10), xscale=log10, xminorticks=IntervalsBetween(10), yminorgridvisible=true, yminorticksvisible=true, xminorgridvisible=true, xminorticksvisible=true)
-    stddevi = Makie.poly!(axc, Rect(statsLG.mean - statsLG.stddev, 0.0, 2 * statsLG.stddev, 0.2), color=(:orange, 0.3))
-    Makie.hist!(axc, time_LG[time_LG.<1e-1], normalization=:probability, bins=80)
-    moy = Makie.lines!(axc, statsLG.mean * [1, 1], [0.0, 0.2], linewidth=3, linestyle=:dot, color=:red)
-    #axislegend(axb, [stddevi,moy], ["95% confidence interval","Mean"], "", position = :rb,
-    #    orientation = :vertical,labelsize=13,framevisible = false)
-    Makie.xlims!(axc, interval)
+    # axc = Axis(ga[3, 1], title="(c) LeafGasExchange.jl", yminorticks=IntervalsBetween(10),
+    #     xscale=log10, xminorticks=IntervalsBetween(10), yminorgridvisible=true, yminorticksvisible=true,
+    #     xminorgridvisible=true, xminorticksvisible=true, xlabel="Time (s)")
+    axc = Axis(ga[3, 1], title="(c) LeafGasExchange.jl", xscale=log10, xlabel="Time (s)", titlealign=:left, titlesize=10)
+    stddevi = poly!(
+        axc,
+        Rect(statsLG.mean - statsLG.stddev, 0.0, 2 * statsLG.stddev, 1),
+        color=(:orange, 0.3), yautolimits=false
+    )
+    vlines!(axc, statsLG.mean; color=:red, linewidth=3, linestyle=:dot)
+    hist!(axc, time_LG, normalization=:probability, bins=bins)
+    xlims!(axc, interval)
 
-    Label(fig[1:3, 0], "Density", rotation=pi / 2, labelsize=25)
-    Label(fig[4, 1:4], "Time (s)")
-
+    rowgap!(ga, 7)
+    hidexdecorations!(axa, grid=false)
+    hidexdecorations!(axb, grid=false)
     fig
 end
 
@@ -210,15 +240,33 @@ end
 # SAVING
 ######################################################################################################
 
-fig = plot_benchmark_Makie(statsPB, statsPE, statsLG)
-save("benchmark_each_time_steps.png", fig, px_per_unit=2) # output size = 1600 x 1200 pixels
+# Save the figure:
+fig = plot_benchmark_Makie(statsPB, statsPE, statsLG, time_PB, time_PE, time_LG)
+save("benchmark_each_time_steps.png", fig, px_per_unit=3)
 
+# Write overall timings:
 df = DataFrame(
-    [getfield(j, i) for i in fieldnames(stat_res), j in [statsPB, statsPE, statsLG]],
+    [getfield(j, i) for i in fieldnames(StatResults), j in [statsPB, statsPE, statsLG]],
     ["PlantBiophysics", "plantecophys", "LeafGasExchange"]
 )
-insertcols!(df, 1, :Stat => [fieldnames(stat_res)...])
+insertcols!(df, 1, :Stat => [fieldnames(StatResults)...])
 CSV.write("benchmark.csv", df)
+
+# Write timing for each sample:
+CSV.write("benchmark_full.csv",
+    DataFrame(
+        "package" => vcat(
+            [
+                repeat([i.first], length(i.second)) for i in [
+                    "PlantBiophysics" => time_PB,
+                    "plantecophys" => time_PE,
+                    "LeafGasExchange" => time_LG
+                ]
+            ]...
+        ),
+        "sample_time" => vcat(time_PB, time_PE, time_LG)
+    )
+)
 
 
 ######################################################################################################
@@ -230,99 +278,99 @@ CSV.write("benchmark.csv", df)
 # we don't have the variability between simulations
 
 # Parameters :
-Random.seed!(1)
-microbenchmark_steps2 = 1000 # Number of times the microbenchmark is run
-microbenchmark_evals2 = 1 # Number of times each sample is run to be sure of the output
-N2 = 100        # Number of timesteps simulated for each microbenchmark step
+# Random.seed!(1)
+# microbenchmark_steps2 = 1000 # Number of times the microbenchmark is run
+# microbenchmark_evals2 = 1 # Number of times each sample is run to be sure of the output
+# N2 = 100        # Number of timesteps simulated for each microbenchmark step
 
-# Create the randomly drawn input set
-set2 = [rand.(vars) for i in 1:N2]
-set2 = reshape(vcat(set2...), (length(set2[1]), length(set2)))'
-name = ["T", "Wind", "P", "Rh", "Ca", "JMaxRef", "VcMaxRef", "RdRef", "Rs", "sky_fraction", "d", "TPURef", "g0", "g1"]
-set2 = DataFrame(set2, name)
-@. set2[!, :vpd] = e_sat(set2.T) - vapor_pressure(set2.T, set2.Rh)
-@. set2[!, :PPFD] = set2.Rs * 0.48 * 4.57
+# # Create the randomly drawn input set
+# set2 = [rand.(vars) for i in 1:N2]
+# set2 = reshape(vcat(set2...), (length(set2[1]), length(set2)))'
+# name = ["T", "Wind", "P", "Rh", "Ca", "JMaxRef", "VcMaxRef", "RdRef", "Rs", "sky_fraction", "d", "TPURef", "g0", "g1"]
+# set2 = DataFrame(set2, name)
+# @. set2[!, :vpd] = e_sat(set2.T) - vapor_pressure(set2.T, set2.Rh)
+# @. set2[!, :PPFD] = set2.Rs * 0.48 * 4.57
 
-# Make variables available to the R session
-@rput set2 N2 microbenchmark_steps2
-R"""
-m2 = microbenchmark(
-    PhotosynEB(
-        Tair = set2$T, VPD = set2$vpd, Wind = set2$Wind,
-        Wleaf = set2$d, Ca = set2$Ca,  StomatalRatio = 1,
-        LeafAbs = set2$sky_fraction, gsmodel = "BBOpti", g0 = set2$g0, g1 = set2$g1,
-        alpha = 0.24, theta = 0.7, Jmax = set2$JMaxRef,
-        Vcmax = set2$VcMaxRef, TPU = set2$TPURef, Rd = set2$RdRef,
-        RH = set2$Rh, PPFD=set2$PPFD, Patm = set2$P
-    ),
-    times = microbenchmark_steps2
-)
+# # Make variables available to the R session
+# @rput set2 N2 microbenchmark_steps2
+# R"""
+# m2 = microbenchmark(
+#     PhotosynEB(
+#         Tair = set2$T, VPD = set2$vpd, Wind = set2$Wind,
+#         Wleaf = set2$d, Ca = set2$Ca,  StomatalRatio = 1,
+#         LeafAbs = set2$sky_fraction, gsmodel = "BBOpti", g0 = set2$g0, g1 = set2$g1,
+#         alpha = 0.24, theta = 0.7, Jmax = set2$JMaxRef,
+#         Vcmax = set2$VcMaxRef, TPU = set2$TPURef, Rd = set2$RdRef,
+#         RH = set2$Rh, PPFD=set2$PPFD, Patm = set2$P
+#     ),
+#     times = microbenchmark_steps2
+# )
 
-    time_PE2 = m2$time / N2 * 10e-9 # transform in seconds
-"""
-@rget time_PE2
-
-
-# Configuration:
-leaves = [
-    begin
-        LeafModels(
-            energy=Monteith(),
-            photosynthesis=Fvcb(VcMaxRef=set2.VcMaxRef[i], JMaxRef=set2.JMaxRef[i], RdRef=set2.RdRef[i], TPURef=set2.TPURef[i]),
-            stomatal_conductance=Medlyn(set2.g0[i], set2.g1[i]),
-            Rₛ=set2.Rs[i], sky_fraction=set2.sky_fraction[i],
-            PPFD=set2.PPFD[i], d=set2.d[i]
-        )
-    end for i in 1:size(set2, 1)
-]
-
-meteos = [
-    begin
-        Atmosphere(T=set2.T[i], Wind=set2.Wind[i], P=set2.P[i], Rh=set2.Rh[i], Cₐ=set2.Ca[i])
-    end for i in 1:size(set2, 1)
-]
-
-function eval_PB(leaves, meteos)
-    for i in 1:N2
-        energy_balance!(leaves[i], meteos[i])
-    end
-end
-
-b_PB2 = @benchmark eval_PB($leaves, $meteos) evals = microbenchmark_evals2 samples = microbenchmark_steps2
-time_PB2 = b_PB2.times ./ N2 .* 1e-9 # transform in seconds
+#     time_PE2 = m2$time / N2 * 10e-9 # transform in seconds
+# """
+# @rget time_PE2
 
 
-# For LeafGasExchange:
-# This is usefull to know the overhead cost from Julia to Python and back.
-configs = [
-    begin
-        :Weather => (
-            PFD=set2.PPFD[i],
-            CO2=set2.Ca[i],
-            RH=set2.Rh[i] * 100,
-            T_air=set2.T[i],
-            wind=set2.Wind[i],
-            P_air=set2.P[i],
-            g0=set2.g0[i],
-            g1=set2.g1[i],
-            Vcmax=set2.VcMaxRef[i],
-            Jmax=set2.JMaxRef[i],
-            Rd=set2.RdRef[i],
-            TPU=set2.TPURef[i]
-        )
-    end for i in 1:size(set2, 1)
-]
-b_LG2 = @benchmark simulate($ModelC3MD; configs=$configs) evals = microbenchmark_evals2 samples = microbenchmark_steps2
-time_LG2 = b_LG2.times ./ N2 .* 1e-9 # transform in seconds
+# # Configuration:
+# leaves = [
+#     begin
+#         LeafModels(
+#             energy=Monteith(),
+#             photosynthesis=Fvcb(VcMaxRef=set2.VcMaxRef[i], JMaxRef=set2.JMaxRef[i], RdRef=set2.RdRef[i], TPURef=set2.TPURef[i]),
+#             stomatal_conductance=Medlyn(set2.g0[i], set2.g1[i]),
+#             Rₛ=set2.Rs[i], sky_fraction=set2.sky_fraction[i],
+#             PPFD=set2.PPFD[i], d=set2.d[i]
+#         )
+#     end for i in 1:size(set2, 1)
+# ]
+
+# meteos = [
+#     begin
+#         Atmosphere(T=set2.T[i], Wind=set2.Wind[i], P=set2.P[i], Rh=set2.Rh[i], Cₐ=set2.Ca[i])
+#     end for i in 1:size(set2, 1)
+# ]
+
+# function eval_PB(leaves, meteos)
+#     for i in 1:N2
+#         energy_balance!(leaves[i], meteos[i])
+#     end
+# end
+
+# b_PB2 = @benchmark eval_PB($leaves, $meteos) evals = microbenchmark_evals2 samples = microbenchmark_steps2
+# time_PB2 = b_PB2.times ./ N2 .* 1e-9 # transform in seconds
 
 
-######################################################################################################
-# SAVING
-######################################################################################################
+# # For LeafGasExchange:
+# # This is usefull to know the overhead cost from Julia to Python and back.
+# configs = [
+#     begin
+#         :Weather => (
+#             PFD=set2.PPFD[i],
+#             CO2=set2.Ca[i],
+#             RH=set2.Rh[i] * 100,
+#             T_air=set2.T[i],
+#             wind=set2.Wind[i],
+#             P_air=set2.P[i],
+#             g0=set2.g0[i],
+#             g1=set2.g1[i],
+#             Vcmax=set2.VcMaxRef[i],
+#             Jmax=set2.JMaxRef[i],
+#             Rd=set2.RdRef[i],
+#             TPU=set2.TPURef[i]
+#         )
+#     end for i in 1:size(set2, 1)
+# ]
+# b_LG2 = @benchmark simulate($ModelC3MD; configs=$configs) evals = microbenchmark_evals2 samples = microbenchmark_steps2
+# time_LG2 = b_LG2.times ./ N2 .* 1e-9 # transform in seconds
 
-statsPB2 = basic_stat(time_PB2)
-statsPE2 = basic_stat(time_PE2)
-statsLG2 = basic_stat(time_LG2)
 
-plot_benchmark(statsPB2, statsPE2, statsLG2)
-savefig("benchmark_all_steps.png")
+# ######################################################################################################
+# # SAVING
+# ######################################################################################################
+
+# statsPB2 = basic_stat(time_PB2)
+# statsPE2 = basic_stat(time_PE2)
+# statsLG2 = basic_stat(time_LG2)
+
+# plot_benchmark(statsPB2, statsPE2, statsLG2)
+# savefig("benchmark_all_steps.png")
